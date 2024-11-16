e1b2ec286f99261be42d8d04b17fb59c64cf82cb
Sean Cheng <seanc2841@gmail.com>
2024-11-16 15:45:43 -0500

    Finish code and FT and wavelet

--- correct_signal.py
+++ correct_signal.py
@@ -1,6 +1,9 @@
 import pywt
+from scipy.fft import fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
+
+time = np.linspace(0, 10, 10000)
 
 # Create a sample signal
 # t_red = np.linspace(0, 5, num=500)
@@ -12,39 +15,58 @@ import matplotlib.pyplot as plt
 
 
 def signal(t):
-    if 0 <= t <= 5:
+    if t <= 5:
         return np.sin(2 * np.pi * t)
     elif 5 <= t <= 9:
         return np.sin(8 * np.pi * t)
     else:
         return np.sin(4 * np.pi * t)
 
+# FT OF SIGNAL
 
-x_red = np.linspace(0, 5, 5000)
-x_green = np.linspace(5, 9, 4000)
-x_yellow = np.linspace(9, 10, 1000)
-y_red = np.array([signal(t) for t in x_red])
-y_green = np.array([signal(t) for t in x_green])
-y_yellow = np.array([signal(t) for t in x_yellow])
-
-plt.plot(x_red, y_red, color='red')
-plt.plot(x_green, y_green, color='green')
-plt.plot(x_yellow, y_yellow, color='yellow')
-# plt.axhline(0, color='black', lw=0.5, ls='--')
-# plt.axvline(0, color='black', lw=0.5, ls='--')
-plt.xticks([])
-plt.yticks([])
-plt.show()
-
-# # Perform CWT
-# scales = np.arange(1, 128)
-# wavelet = 'morl'
-# coef, freqs = pywt.cwt(y_vals, scales, wavelet)
-#
-# # Plot the CWT
-# plt.imshow(coef, extent=[0, 1, 1, 128], cmap='viridis', aspect='auto', interpolation='nearest')
-# plt.colorbar(label='CWT Coefficients')
-# plt.xlabel('Time')
-# plt.ylabel('Scale')
-# plt.title('Continuous Wavelet Transform')
+# x = np.linspace(0, 10, 10000)
+# y = fft([signal(t) for t in x])
+# freqs = fftfreq(len(x), x[1] - x[0])
+# plt.plot(freqs, np.abs(y), color='blue')
+# plt.xlabel("Frequency")
+# plt.ylabel("Magnitude")
+# plt.xlim(-10, 10)
 # plt.show()
+
+
+# RAW SIGNAL
+
+# x_red = np.linspace(0, 5, 5000)
+# x_green = np.linspace(5, 9, 4000)
+# x_yellow = np.linspace(9, 10, 1000)
+# y_red = np.array([signal(t) for t in x_red])
+# y_green = np.array([signal(t) for t in x_green])
+# y_yellow = np.array([signal(t) for t in x_yellow])
+#
+# plt.plot(x_red, y_red, color='red')
+# plt.plot(x_green, y_green, color='green')
+# plt.plot(x_yellow, y_yellow, color='yellow')
+# # plt.axhline(0, color='black', lw=0.5, ls='--')
+# # plt.axvline(0, color='black', lw=0.5, ls='--')
+# plt.xticks([])
+# plt.yticks([])
+# plt.show()
+
+
+# CWT
+scales = np.geomspace(128, 1536, num=100)
+x_vals = [signal(t) for t in time]
+wavelet = 'cmor1.5-1.0'
+sampling_period = np.diff(time).mean()
+coef, freqs = pywt.cwt(x_vals, scales, wavelet, sampling_period=sampling_period)
+coef = np.abs(coef[:-1, :-1])
+
+# Plot the CWT
+fig, axs = plt.subplots(1, 1)
+pcm = axs.pcolormesh(time, freqs, coef)
+axs.set_yscale("log", base=2)
+axs.set_xlabel("Time (s)")
+axs.set_ylabel("Frequency (Hz)")
+axs.set_title("Wavelet Transform of Correct Signal")
+fig.colorbar(pcm, ax=axs)
+plt.show()
--- malfunctioning_signal.py
+++ malfunctioning_signal.py
@@ -1,5 +1,9 @@
+import pywt
+from scipy.fft import fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
+
+time = np.linspace(0, 10, 10000)
 
 def signal(t):
     if 0 <= t <= 5:
@@ -10,29 +14,43 @@ def signal(t):
         return np.sin(8 * np.pi * t)
 
 
-x_red = np.linspace(0, 5, 5000)
-x_yellow = np.linspace(5, 6, 4000)
-x_green = np.linspace(6, 10, 1000)
-y_red = np.array([signal(t) for t in x_red])
-y_yellow = np.array([signal(t) for t in x_yellow])
-y_green = np.array([signal(t) for t in x_green])
+# x = np.linspace(0, 10, 10000)
+# y = fft([signal(t) for t in x])
+# freqs = fftfreq(len(x), x[1] - x[0])
+# plt.plot(freqs, np.abs(y), color='red')
+# plt.xlabel("Frequency")
+# plt.ylabel("Magnitude")
+# plt.xlim(-10, 10)
+# plt.show()
 
-plt.plot(x_red, y_red, color='red')
-plt.plot(x_green, y_green, color='green')
-plt.plot(x_yellow, y_yellow, color='yellow')
-plt.xticks([])
-plt.yticks([])
-plt.show()
+# x_red = np.linspace(0, 5, 5000)
+# x_yellow = np.linspace(5, 6, 4000)
+# x_green = np.linspace(6, 10, 1000)
+# y_red = np.array([signal(t) for t in x_red])
+# y_yellow = np.array([signal(t) for t in x_yellow])
+# y_green = np.array([signal(t) for t in x_green])
+#
+# plt.plot(x_red, y_red, color='red')
+# plt.plot(x_green, y_green, color='green')
+# plt.plot(x_yellow, y_yellow, color='yellow')
+# plt.xticks([])
+# plt.yticks([])
+# plt.show()
 
 # # Perform CWT
-# scales = np.arange(1, 128)
-# wavelet = 'morl'
-# coef, freqs = pywt.cwt(y_vals, scales, wavelet)
-#
-# # Plot the CWT
-# plt.imshow(coef, extent=[0, 1, 1, 128], cmap='viridis', aspect='auto', interpolation='nearest')
-# plt.colorbar(label='CWT Coefficients')
-# plt.xlabel('Time')
-# plt.ylabel('Scale')
-# plt.title('Continuous Wavelet Transform')
-# plt.show()
+scales = np.geomspace(128, 1536, num=100)
+x_vals = [signal(t) for t in time]
+wavelet = 'cmor1.5-1.0'
+sampling_period = np.diff(time).mean()
+coef, freqs = pywt.cwt(x_vals, scales, wavelet, sampling_period=sampling_period)
+coef = np.abs(coef[:-1, :-1])
+
+# Plot the CWT
+fig, axs = plt.subplots(1, 1)
+pcm = axs.pcolormesh(time, freqs, coef)
+axs.set_yscale("log", base=2)
+axs.set_xlabel("Time (s)")
+axs.set_ylabel("Frequency (Hz)")
+axs.set_title("Wavelet Transform of Malfunctioning Signal")
+fig.colorbar(pcm, ax=axs)
+plt.show()
